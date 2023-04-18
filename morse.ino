0c0ac5850e1cab3d4f064eddd51fd2ee2451e85e
oselcukyatagan <115888665+oselcukyatagan@users.noreply.github.com>
2023-04-18 23:14:09 +0300

    mostly done

--- morse.ino
+++ morse.ino
@@ -311,104 +311,128 @@ void loop(){
       case 'C':
         C();
         delay(letterBreak);
-          
+        Serial.println("C");
         break;
       case 'D':
         D();
         delay(letterBreak);
+        Serial.println("D");
         break;
       case 'E':
         E();
         delay(letterBreak);
+        Serial.println("E");
         break;
   /*
       case 'F':
           F();
           delay(letterBreak);
+          Serial.println("F");
           break;
   */
       case 'G':
         G();
         delay(letterBreak);
+        Serial.println("G");
         break;
       case 'H':
         H();
         delay(letterBreak);
+        Serial.println("H");
         break;
       case 'I':
         I();
         delay(letterBreak);
+        Serial.println("I");
         break;
       case 'J':
         J();
         delay(letterBreak);
+        Serial.println("J");
         break;
       case 'K':
         K();
         delay(letterBreak);
+        Serial.println("K");
         break;
       case 'L':
         L();
         delay(letterBreak);
+        Serial.println("L");
         break;
       case 'M':
         M();
         delay(letterBreak);
+        Serial.println("M");
         break;
       case 'N':
         N();
         delay(letterBreak);
+        Serial.println("N");
         break;
       case 'O':
         O();
         delay(letterBreak);
+        Serial.println("O");
         break;
       case 'P':
         P();
         delay(letterBreak);
+        Serial.println("P");
         break;
       case 'Q':
         Q();
         delay(letterBreak);
+        Serial.println("Q");
         break;
       case 'R':
         R();
         delay(letterBreak);
+        Serial.println("R");
         break;
       case 'S':
         S();
         delay(letterBreak);
+        Serial.println("S");
         break;
       case 'T':
         T();
         delay(letterBreak);
+        Serial.println("T");
         break;
       case 'U':
         U();
         delay(letterBreak);
+        Serial.println("U");
         break;
       case 'V':
         V();
         delay(letterBreak);
+        Serial.println("V");
         break;
       case 'W':
         W();
         delay(letterBreak);
+        Serial.println("W");
         break;
       case 'X':
         X();
         delay(letterBreak);
+        Serial.println("X");
         break;
       case 'Y':
         Y();
         delay(letterBreak);
+        Serial.println("Y");
         break;
       case 'Z':
         Z();
         delay(letterBreak);
+        Serial.println("Z");
         break;
       case ' ':
         delay(wordBreak);   
+        Serial.println(" ");
       default:
         Serial.print("");
         break;
